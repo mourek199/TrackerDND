ccc72481a9ec20b316cf2af9792ffec21631afb6
mourek199 <antoninfranek199@gmail.com>
2026-04-27 11:59:04 +0200

    Startup button has startup text

--- src/StartupMenu.java
+++ src/StartupMenu.java
@@ -9,15 +9,18 @@ public class StartupMenu extends JFrame {
     public StartupMenu(){
         super("startup");
 
-        this.setSize(400, 100);
+        this.setSize(800, 200);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.setLayout(new BorderLayout());
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-        ImageIcon startIcon = new ImageIcon("res/tapir.png");
+        ImageIcon startIcon = new ImageIcon("res/startupTest.png");
         button = new JButton(startIcon);
         this.add(button, BorderLayout.CENTER);
+        button.addActionListener(e -> {
+            this.dispose();
+        });
 
         this.setVisible(true);
     }
