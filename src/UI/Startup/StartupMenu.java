f9fb52538b139dd30933103be7fc1e8ae000a026
mourek199 <antoninfranek199@gmail.com>
2026-05-21 19:01:32 +0200

    I am starting to understand this

--- src/UI/Startup/StartupMenu.java
+++ src/UI/Startup/StartupMenu.java
@@ -1,23 +1,28 @@
 package UI.Startup;
 
 import UI.Background;
-import UI.CustomButton;
-import UI.WindowMain.MainWindow;
 
+import javax.imageio.ImageIO;
+import javax.sound.sampled.BooleanControl;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 
 public class StartupMenu extends JFrame {
 
     private JPanel panel;
+    private JPanel panel2;
     private StartupButton button;
+    private StartupButton button2;
     private Background bg;
 
     public StartupMenu(){
         super("startup");
 
-        this.setSize(600, 100);
+        this.setSize(1000, 750);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
@@ -32,15 +37,42 @@ public class StartupMenu extends JFrame {
         }
 
         JPanel panel = new JPanel();
+        JPanel panel2 = new JPanel();
+
+        try {
+            File f = new File("res/dndLogo.png");
+            BufferedImage myPicture = ImageIO.read(f);
+            Image scaledImg = myPicture.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+            JLabel picLabel = new JLabel(new ImageIcon(scaledImg));
+            panel2.add(picLabel);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        bg.add(panel2);
         bg.add(panel);
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setOpaque(false);
 
-        Dimension panel2Dimension = new Dimension(30,30);
-        button = new StartupButton("/dndLogo.png", "/dndLogo.png", 40,40);
+        panel2.setLayout(new BoxLayout(panel2, BoxLayout.Y_AXIS));
+        panel2.setOpaque(false);
+
+        Dimension panelDimension = new Dimension(30,30);
+        button = new StartupButton("/TrackerLogo.png", "/dndLogo.png", 610,100);
         button.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(Box.createVerticalStrut(10));
         panel.add(button);
+
+        Dimension panel2Dimension = new Dimension(this.getWidth(), 100);
+        panel2.add(Box.createVerticalStrut(10));
+        panel2.setPreferredSize(panel2Dimension);
+        panel2.setAlignmentY(Component.CENTER_ALIGNMENT);
+        panel2.add(new JButton("daaamn"));
+
+
+
+
 
         this.setVisible(true);
     }
